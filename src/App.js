adcd88cf0ec4ce16553c3eed34a06c53f9ab5910
Karthik-Vempati <karvempati926@gmail.com>
2022-07-29 21:47:05 +0530

    First

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Login from './components/Login';
+
 function App() {
   return (
     <div>
-      <h1>Let's Go</h1>
+      <Login />
     </div>
   );
 }
---
+++ src/components/Login.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import Header from './Header';
+import {useState} from 'react';
+
+function Login() {
+  const [email,setEmail] = useState('');
+  const [password,setPassword] = useState('');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    //login function
+    const login = (e) =>{
+        e.preventDefault();
+        const user = {
+            email,
+            password,
+        };
+        localStorage.setItem('token-info',JSON.stringify(user));
+        setIsLoggedIn(true);
+        setEmail('');
+        setPassword('');
+    };
+
+    const logout = () =>{
+        localStorage.removeItem('token-info');
+        setIsLoggedIn(false);
+    };
+
+  return (
+      <div>
+        <Header />
+        <h1>Login</h1>
+        {!isLoggedIn ? (
+            <>
+                <form action="">
+                    <input type="email" onChange={(e) => setEmail(e.target.value)} value={email} placeholder="Email Address"/>
+                <input type="password" onChange={(e) => setPassword(e.target.value)} value={password} placeholder="Password" />
+                <a>New User? Create an account</a>
+                <button type="submit" onClick={login}>Submit</button>
+                </form>
+            </>
+        ): (
+            <>
+
+    <h1>User Logged In</h1>
+    <button onClickCapture={logout}>logout user</button>
+            </>
+
+  )}
+    </div>
+  ) 
+}
+
+export default Login;
